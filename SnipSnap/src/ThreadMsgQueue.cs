03aaf892574384dfd9e13ca3ee719f1b861c672c
Douglas Horn <douglas.j.horn@gmail.com>
2012-09-23 09:09:46 -0700

    Switch to BlockingCollection<T> as per lzm

--- SnipSnap/src/ThreadMsgQueue.cs
+++ SnipSnap/src/ThreadMsgQueue.cs
@@ -1,34 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 
 namespace SnipSnap
 {
     public static class ThreadMsgQueue<T>
     {
-        private static Queue<T> messages = new Queue<T>();
+        private static BlockingCollection<T> messages = new BlockingCollection<T>();
 
         public static void Enqueue(T enq)
         {
-            lock (messages)
-            {
-                Console.WriteLine("Enq: " + enq.ToString());
-                messages.Enqueue(enq);
-            }
+            messages.Add(enq);
         }
 
         public static T Dequeue()
         {
-            lock (messages)
-            {
-                if (messages.Count == 0) 
-                   // TODO
-                   // return default(T);  
-
-                T ret = messages.Dequeue();
-                Console.WriteLine("Deq: " + ret);
-                //return messages.Dequeue();
-                return ret;
-            }
+            return messages.Take();            
         }
     }
 }
+
